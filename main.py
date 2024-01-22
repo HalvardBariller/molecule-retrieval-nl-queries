09b75eaabf985c0175d118d70fd608622616b3e3
vdng9338 <vdng9338@users.noreply.github.com>
2024-01-22 16:36:48 +0100

    Make switching between graph and text encoders more convenient

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from torch_geometric.loader import DataLoader
 from torch.utils.data import DataLoader as TorchDataLoader
 from sklearn.metrics import label_ranking_average_precision_score
 from models.Model import Model
+from models.graph_encoders import GraphEncoder, GraphormerEncoder
+from models.text_encoders import TextEncoder
 import numpy as np
 from transformers import AutoTokenizer
 import torch
@@ -62,7 +64,9 @@ num_workers = 12
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers = num_workers, pin_memory = True)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers = num_workers, pin_memory = True)
 
-model = Model(model_name=model_name, num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300) # nout = bert model hidden dim
+graph_encoder = GraphEncoder(num_node_features=300, nout=768, nhid=300, graph_hidden_channels=300) # nout = bert model hidden dim
+text_encoder = TextEncoder(model_name)
+model = Model(graph_encoder, text_encoder)
 model.to(device)
 
 optimizer = optim.AdamW(model.parameters(), lr=learning_rate,
