8b22d194ac1d0e3e7b25a409d58f0c4d6fb2452c
Halvard Bariller <halvard.bariller@universite-paris-saclay.fr>
2024-01-30 13:28:54 +0100

    Temperature parameter from CLIP

--- losses/contrastive_loss.py
+++ losses/contrastive_loss.py
@@ -10,7 +10,7 @@ def contrastive_loss(v1, v2):
 
   
 def contrastive_loss_with_cosine(v1, v2):
-  logits = torch.matmul(v1,torch.transpose(v2, 0, 1)) * np.exp(1)
+  logits = torch.matmul(v1,torch.transpose(v2, 0, 1)) * np.exp(0.07)
   v1_norm = v1 / v1.norm(dim=1)[:, None]
   v2_norm = v2 / v2.norm(dim=1)[:, None]
   logits_cosine = torch.matmul(v1_norm,torch.transpose(v2_norm, 0, 1))
